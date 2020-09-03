5d8aedcca5acf7e554279203edb5f946fa1ffb82
VBV-121 <vghadiali18@gmail.com>
2020-09-03 21:35:36 +0530

    Initial Change

---
+++ flaskblog/__init__.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY']='1a858e5d5f93ac4338efe291b34f9d8c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER']='smtp.googlemail.com'
+app.config['MAIL_PORT']=587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME']='vghadiali18@gmail.com'
+app.config['MAIL_PASSWORD']='yhcbcxyfdjcodwre'
+mail=Mail(app)
+
+
+from flaskblog import routes
